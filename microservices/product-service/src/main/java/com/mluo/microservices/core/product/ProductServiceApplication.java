f85ee41642e096d946df09b0c06fb03fef73d487
Michael Luo <michael.luo0801@gmail.com>
2020-03-26 10:26:44 +1100

    update project settings

--- microservices/product-service/src/main/java/com/mluo/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/mluo/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.mluo.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.mluo")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
