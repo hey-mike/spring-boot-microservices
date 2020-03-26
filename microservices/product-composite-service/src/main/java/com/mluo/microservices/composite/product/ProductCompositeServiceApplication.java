39c01ed6a314adcfcd07d5372af51bc8a99e9405
Michael Luo <michael.luo0801@gmail.com>
2020-03-26 18:18:37 +1100

    add aggreation service

--- microservices/product-composite-service/src/main/java/com/mluo/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/mluo/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -2,9 +2,16 @@ package com.mluo.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
