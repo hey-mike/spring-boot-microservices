d2fa885d627b02e4b0920c15d11c660e8dcb5390
Michael Luo <michael.luo0801@gmail.com>
2020-03-26 18:24:26 +1100

    add composite servier

--- microservices/product-composite-service/src/main/java/com/mluo/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/mluo/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -3,9 +3,11 @@ package com.mluo.microservices.composite.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("com.mluo")
 public class ProductCompositeServiceApplication {
 
 	@Bean
