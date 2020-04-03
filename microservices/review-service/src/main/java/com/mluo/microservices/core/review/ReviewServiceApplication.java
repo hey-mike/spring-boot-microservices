0648c4c7674ed5fdd3df084d2c3bcc41be08e1f7
Michael Luo <michael.luo0801@gmail.com>
2020-04-03 16:16:34 +1100

    fix dependencies

--- microservices/review-service/src/main/java/com/mluo/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/mluo/microservices/core/review/ReviewServiceApplication.java
@@ -2,7 +2,9 @@ package com.mluo.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.mluo")
 @SpringBootApplication
 public class ReviewServiceApplication {
 
