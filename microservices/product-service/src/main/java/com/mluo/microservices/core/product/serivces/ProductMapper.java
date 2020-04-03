c2bfb7636128aa01995e15b19b697aa52592ed36
Michael Luo <michael.luo0801@gmail.com>
2020-04-03 22:22:47 +1100

    object mapper

---
+++ microservices/product-service/src/main/java/com/mluo/microservices/core/product/serivces/ProductMapper.java
@@ -0,0 +1,22 @@
+package com.mluo.microservices.core.product.serivces;
+
+import com.mluo.api.core.product.Product;
+import com.mluo.microservices.core.product.persistance.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(target = "serviceAddress", ignore = true)
+    })
+    Product entityToApi(ProductEntity entity);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "version", ignore = true)
+    })
+    ProductEntity apiToEntity(Product api);
+}
